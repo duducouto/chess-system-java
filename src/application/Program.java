7ccb22eedaf47102da17b726055ebeba8d3fa711
Eduardo Couto <eduardo.couto.rj@hotmail.com>
2023-05-31 16:39:38 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		Position post = new Position (3, 4);
+		
+		System.out.println(post);
 	}
 
 }
