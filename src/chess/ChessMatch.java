4b120388590029f387c7b03bc30aedff80866979
Eduardo Couto <eduardo.couto.rj@hotmail.com>
2023-06-06 17:46:24 -0300

    Pawn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
